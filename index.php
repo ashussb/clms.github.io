b81c8ca0918ac753308dd4fb9f00b7c8c29163a7
ashussb <100453642+ashussb@users.noreply.github.com>
2022-02-26 07:59:52 +0530

    my first website

---
+++ index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+  
+    <div class="container">
+        
+    <?php echo"this is my first website";
+
+    $variable1=45;
+    $variable2=45;
+
+    echo $variable1 + $variable2;
+
+    echo " The value of $variable1 + $variable2 is";
+    echo    "<br>";
+    echo $variable1 + $variable2;
+    
+    ?>
+    <?php
+    
+    ?>
+    
+    </div>
+</body>
+</html>
